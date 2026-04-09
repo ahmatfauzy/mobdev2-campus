f0ee9a388d1d597fa0414bab0d5de01b4c46f932
ahmatfauzy <ahmatf703@gmail.com>
2026-04-09 20:15:20 +0700

    feat: penambahan decrement, dan penambahan penggunaan 8 Utility GetX

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:get/get_state_manager/get_state_manager.dart';
-import 'package:get/instance_manager.dart';
-import 'package:get/route_manager.dart';
-import 'package:get/state_manager.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,31 +10,88 @@ class CounterController extends GetxController {
 
   void increment() {
     count++;
+    Get.snackbar(
+      'Sukses',
+      'Nilai bertambah menjadi $count',
+      snackPosition: SnackPosition.BOTTOM,
+      backgroundColor: Colors.green[100],
+      duration: Duration(seconds: 1),
+    );
+  }
+
+  void decrement() {
+    if (count > 0) {
+      count--;
+    } else {
+      Get.snackbar(
+        'Peringatan',
+        'Nilai sudah 0!',
+        snackPosition: SnackPosition.TOP,
+        backgroundColor: Colors.red[100],
+      );
+    }
   }
 }
 
 class MyApp extends StatelessWidget {
-  CounterController counterController = Get.put(CounterController());
-
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(home: CounterPage());
+    return GetMaterialApp(
+      title: 'Counter GetX',
+      debugShowCheckedModeBanner: false,
+      home: CounterPage(),
+    );
   }
 }
 
 class CounterPage extends StatelessWidget {
-  const CounterPage({super.key});
+  CounterPage({super.key}) {
+    Get.put(CounterController());
+  }
 
   CounterController get controller => Get.find();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Counter PAge")),
-      body: Center(child: Obx(() => Text("Nilai : ${controller.count}"))),
+      appBar: AppBar(title: Text("Counter GetX")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Counter dengan GetX"),
+            SizedBox(height: 20),
+            Obx(
+              () => Text(
+                "${controller.count}",
+                style: TextStyle(fontSize: 60, fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FloatingActionButton(
+                  onPressed: controller.decrement,
+                  heroTag: "decrement",
+                  backgroundColor: Colors.red,
+                  child: Icon(Icons.remove),
+                ),
+                SizedBox(width: 20),
+                FloatingActionButton(
+                  onPressed: controller.increment,
+                  heroTag: "increment",
+                  backgroundColor: Colors.green,
+                  child: Icon(Icons.add),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: controller.increment,
-        child: Icon(Icons.add),
+        onPressed: () => Get.back(),
+        child: Icon(Icons.arrow_back),
       ),
     );
   }
